442508bf57e065b490068e64e542a256a773d4cf
Muzzasaurus <99302729+Muzzasaurus@users.noreply.github.com>
2022-07-19 04:27:32 +1200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 /* Default Variables */
-const origprices = [1, 100, 400, 900, 3500, 10000, 36000, 115000, 440000, 1000000, 2800000, 9100000, 22000000, 100000000, 700000000, 1000000000, 3000000000]
-const origrewards = [0.1, 1, 3, 12, 40, 120, 700, 1400, 3000, 5000, 12000, 45000, 100000, 250000, 1500000, -5000000, 4000000]
+const origprices = [1, 100, 400, 900, 3500, 10000, 36000, 115000, 440000, 1000000, 2800000, 9100000, 22000000, 100000000, 700000000, 1000000000, 3000000000, (Math.random() * 1000000) + 10]
+const origrewards = [0.1, 1, 3, 12, 40, 120, 700, 1400, 3000, 5000, 12000, 45000, 100000, 250000, 1500000, -5000000, 4000000, origprices[17] / (Math.random() * 1000) + 0.001]
 const origbusinessPrices = [10, 250, 750, 6500, 21000, 254254, 1000000, 4000000, 25000000, 40000000, 111111111, 254254254, 933748382, 6969696969, 50000000000, 175000000000]
 const origbusinessRewards = [0.1, 1, 3, 10, 70, 600, 1500, 3000, 9000, 15000, 51111, 84254, 187000, 500000, 3750000, 12500000]
 const origisPurchased = [0]
@@ -8,6 +8,8 @@ const origisPurchased = [0]
 h=1;
 newH=0;
 clickPower=0;
+trueClickPower=0;
+startingHPC=0;
 autoH=0;
 trueAutoH=0;
 theme=-1;
@@ -17,7 +19,7 @@ glodH=0;
 glodHNugget=0;
 glodLimit=10;
 glodPower=0;
-glodBoost=0;
+glodBoost=1;
 requirement=1000000000;
 hRemaining=1000000000;
 const themes = ["Default", "Dark", "Reddish", "Blue", "Pastel", "Purple",
@@ -30,7 +32,7 @@ const upgrNum = new Array(rewards.length);
 const flavours = ["Pencil", "Pen", "Printer", "Typewriter", "Keyboard", "Broken Keyboard", "Intern",
 "H Secret Cult", "Employee", "Shady Business Partner", "H File Tactics", "Psychological Warfare",
 "https://docs.google.com/document/d/1roD6zfNKjTEF-W3KmHWMI22GVrTtueUFx__ikHeNCfs/edit?usp=drivesdk",
-"Genetically Modified H Men", "H Colonization", "Subservient Fish (blood for the blood god)", "Dragon Employee"]
+"Genetically Modified H Men", "H Colonization", "Subservient Fish (blood for the blood god)", "Dragon Employee", "Random"]
 upgradeNumber = prices.length;
 
 /* Business Arrays (H Per Click) */
@@ -43,8 +45,8 @@ const businessNames = ["Chocolate H", "Strawberry H", "Crime H", "Lead Poisoning
 businessNumber = businessPrices.length;
 
 /* Glod Shop Arrays */
-const glodPrices = [10, 100, 250]
-const glodRewards = [0.1, 10, 30]
+const glodPrices = [10, 100, 250, 500]
+const glodRewards = [0.1, 10, 30, 1]
 const isPurchased = new Array(origisPurchased.length);
 for (i=0; i<(isPurchased.length); i++) {
 		isPurchased[i]=origisPurchased[i];
@@ -56,8 +58,8 @@ function onStart() {
 	idle();
 }
 function increment() {
-	h+=clickPower;
-	newH+=clickPower;
+	h+=trueClickPower;
+	newH+=trueClickPower;
 }
 function checkIfNegative() {
 	if (h < 0) {
@@ -69,11 +71,11 @@ function idle() {
 	document.getElementById("glodHDisplay").innerHTML=addCommas(Math.round(glodH)) + " Glod H";
 	document.getElementById("glodHNuggetDisplay").innerHTML=addCommas(Math.round(glodHNugget)) + " Glod H Nuggets";
 	document.getElementById("glodHNuggetDisplay2").innerHTML=addCommas(Math.round(glodHNugget)) + " Glod H Nuggets";
-	document.getElementById("clickDisplay").innerHTML=addCommas(Math.round(clickPower*10)/10) + " H/c";
+	document.getElementById("clickDisplay").innerHTML=addCommas(Math.round(trueClickPower*10)/10) + " H/c";
 	document.getElementById("resetButton").innerHTML="Reset for " + addCommas(potentialGlod) + " Glod H (Limit: " + addCommas(glodLimit) +")";
 	document.getElementById("glodCountdown").innerHTML=addCommas(Math.round(hRemaining)) + " Hs left.";
 	for (i = 0; i < upgradeNumber; i++) {
-		document.getElementById(`hUpgrade${i+1}`).innerHTML=flavours[i] + " (" + addCommas(upgrNum[i]) + ")<br>(+" + addCommas(Math.round((rewards[i]*glodBoost)*10)/10) + " H/s)<br><br>" + addCommas(Math.round(prices[i]*10)/10) + " Hs";
+		document.getElementById(`hUpgrade${i+1}`).innerHTML=flavours[i] + " (" + addCommas(upgrNum[i]) + ")<br>(+" + addCommas(Math.round((rewards[i]*glodBoost)*10)/10) + " H/s)<br><br>" + addCommas(Math.round(prices[i]*1)/1) + " Hs";
 	}
 	for (i = 0; i < businessNumber; i++) {
 		document.getElementById(`hBusiness${i+1}`).innerHTML=businessNames[i] + " (" + addCommas(businessUpgrNum[i]) + ")<br>(+" + addCommas(Math.round((businessRewards[i]*glodBoost)*10)/10) + " H/c)<br><br><br><br>" + addCommas(Math.round(businessPrices[i])) + " Hs";
@@ -87,6 +89,7 @@ function idle() {
 	newH+=(trueAutoH/40);
 	glodBoost=(glodH*glodPower)+1;
 	trueAutoH=autoH*glodBoost;
+	trueClickPower=clickPower*glodBoost;
 	canAfford();
 	potentialCheck();
 	checkIfNegative();
@@ -121,17 +124,17 @@ function canAfford() {
 /* Purchases */
 function purchase(upnum) {
 	h-=prices[upnum-1];
-	autoH+=rewards[upnum-1]*glodBoost;
+	autoH+=rewards[upnum-1];
 	prices[upnum-1]*=1.15;
 	upgrNum[upnum-1]+=1;
 	if (upgrNum[upnum-1] % 10 == 0) {
-		autoH+=upgrNum[upnum-1]*rewards[upnum-1]*glodBoost;
+		autoH+=upgrNum[upnum-1]*rewards[upnum-1];
 		rewards[upnum-1]*=2;
 	}
 }
 function purchaseBusiness(upnum) {
 	h-=businessPrices[upnum-1];
-	clickPower+=businessRewards[upnum-1]*glodBoost;
+	clickPower+=businessRewards[upnum-1];
 	businessPrices[upnum-1]*=1.4;
 	businessUpgrNum[upnum-1]+=1;
 	if (businessUpgrNum[upnum-1] % 5 == 0) {
@@ -147,6 +150,10 @@ function purchaseGlod(upnum, type) {
 	}
 	if (type == "power") {
 		glodPower+=glodRewards[upnum-1];
+	}
+	if (type == "startingHPC") {
+		startingHPC+=glodRewards[upnum-1];
+		clickPower+=glodRewards[upnum-1]/glodBoost;
 	}
 }
 /* Glod H */
@@ -228,10 +235,10 @@ function addCommas(num) {
 	return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 function resetValues() {
-	h=1;
-	newH=0;
-	clickPower=0;
 	autoH=0;
+	trueAutoH=0;
+	clickPower=startingHPC/glodBoost;
+	trueClickPower=0;
 	requirement=1000000000;
 	hRemaining=requirement;
 	for (i=0; i<(prices.length); i++) {
@@ -252,4 +259,6 @@ function resetValues() {
 	for (i=0; i<(businessRewards.length); i++) {
 		businessRewards[i]=origbusinessRewards[i];
 	}
+	h=1;
+	newH=0;
 }
